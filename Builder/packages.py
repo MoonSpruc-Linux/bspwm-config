f3c2ea9db371f06be8f9b2f134f7aaef81450eeb
MoonSpruc-Linux <syndykov100ha@gmail.com>
2024-11-11 21:25:13 +0200

    Update packages.py (remove breeze gnome-calculator ffmpeg imagemagick mat2 dpkg gedit vlc ffmpegthumbnailer ptpython)

--- Builder/packages.py
+++ Builder/packages.py
@@ -1,32 +1,26 @@
 
 
 BASE_PACKAGES = [
-    "tumbler", "ffmpegthumbnailer", "lsd", "alacritty", "calc",
-    "automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "feh", "firefox",
-    "fish", "dpkg", "gcc", "gedit", "git", "gnu-netcat", "htop", "btop", "lxappearance",
-    "mat2", "mpv", "thunar", "neofetch", "network-manager-applet", "nitrogen",
-    "pamixer", "pavucontrol", "polybar", "autoconf", "pulseaudio",
-    "pulseaudio-alsa", "python-pyalsa", "ranger", "redshift", "reflector", "rofi", "rofi-calc", "calcurse",
-    "rofi-emoji", "sudo", "slop", "tree", "unrar", "zip", "unzip", "xarchiver",
-    "xfce4-settings", "zathura", "zathura-djvu", "zathura-pdf-mupdf",
-    "cmake", "clang", "gzip", "imagemagick", "make", "pulseaudio-bluetooth", "shellcheck",
-    "vlc", "usbutils", "picom", "alsa-plugins", "alsa-tools", "alsa-utils", "ffmpeg",
-    "p7zip", "gparted", "xclip", "gpick", "ueberzug", "netctl",
-    "breeze", "amd-ucode", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd"
+    "tumbler", "lsd", "alacritty", "calc", "automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", 
+    "feh", "firefox", "fish", "gcc", "git", "gnu-netcat", "htop", "btop", "lxappearance", "mpv", 
+    "thunar", "neofetch", "network-manager-applet", "nitrogen", "pamixer", "pavucontrol", "polybar", 
+    "autoconf", "pulseaudio", "pulseaudio-alsa", "python-pyalsa", "ranger", "redshift", "reflector", 
+    "rofi", "rofi-calc", "calcurse", "rofi-emoji", "sudo", "slop", "tree", "unrar", "zip", "unzip", "xarchiver", 
+    "xfce4-settings", "zathura", "zathura-djvu", "zathura-pdf-mupdf", "cmake", "clang", "gzip", "make", "pulseaudio-bluetooth", 
+    "shellcheck", "usbutils", "picom", "alsa-plugins", "alsa-tools", "alsa-utils", "p7zip", "gparted", "xclip", "gpick", 
+    "ueberzug", "netctl", "amd-ucode", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd"
 ]
 
 DEV_PACKAGES = [
-    "timeshift", "pinta", "gufw", "python-pywal",
-    "bleachbit", "gnome-firmware", "dconf-editor",
+    "timeshift", "pinta", "gufw", "python-pywal", "bleachbit", "gnome-firmware", "dconf-editor",
     "obs-studio", "tmux", "code", "deluge-gtk", "flameshot", "python-pip"
 ]
 
 AUR_PACKAGES = [
-    "cava", "i3lock-color", "ptpython"
+    "cava", "i3lock-color"
 ]
 
 GNOME_OFFICIAL_TOOLS = [
-    "gnome-calculator", "gnome-disk-utility", "gucharmap",
-    "gthumb", "gnome-clocks"
+    "gnome-disk-utility", "gucharmap", "gthumb", "gnome-clocks"
 ]
 
